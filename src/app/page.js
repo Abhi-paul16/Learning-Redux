bf1cd19fa8210210e291d7bb5d334e07ef0ee577
Abhi-paul16 <abhishek1100861@gmail.com>
2024-03-21 23:27:16 +0530

    bug fixing in redux module

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-
+ 
+import { Provider } from 'react-redux'
+import { store } from "@/store";
+import AddTodo from "@/components/AddTodo";
+import Todos from "@/components/Todos";
 export default function Home() {
   return (
+    <Provider store={store}>
     <main className={styles.main}>
     <h2>Redux toolkit Learning</h2>
+    <AddTodo/>
+    <Todos/>  
     </main>
+    </Provider>
   );
 }
--- src/components/Todos/index.jsx
+++ src/components/Todos/index.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import {useSelector , useDispatch} from "react-redux";
-import { removeTodo } from '@/features/todo/todoSlce';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "@/features/todo/todoSlce";
 
 const Todos = () => {
-    const todos =  useSelector(state => state.todos);// we will get the value in the
+  const todos = useSelector((state) => state.todos); // we will get the value in the
+  const dispatch = useDispatch();
   return (
     <>
-     <div>
-        Todos
-        </div>   
-{todos.map((todo)=>(
-    <li key={todo.id}>
-{todo.text}
-</li>
-)) }
+      <div>Todos</div>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <div>{todo.text}</div> 
+          <button onClick={() => dispatch(removeTodo(todo.id))}>delete</button>
+        </li>
+      ))}
     </>
-    
-  ) 
-}
+  );
+};
 
 export default Todos;
